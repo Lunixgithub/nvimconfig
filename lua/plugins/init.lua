728d876b4588b2a6992fbdb8d78b301eb268a6b8
lutje <luetje.helzel@gmail.com>
2025-07-04 17:57:57 +0200

    added diagnostics

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -91,5 +91,16 @@ return {
       { "mason-org/mason.nvim", opts = {} },
       "neovim/nvim-lspconfig",
     },
-  }
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy", -- Or `LspAttach`
+    priority = 1000, -- needs to be loaded in first
+    config = function()
+        require('tiny-inline-diagnostic').setup()
+        vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    end
 }
+}
+
