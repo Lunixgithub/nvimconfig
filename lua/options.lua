4ca190ffc5216546e8744b5d2ea009e53c3c8511
lutje <luetje.helzel@gmail.com>
2025-07-13 13:01:11 +0200

    added scrolloff

--- lua/options.lua
+++ lua/options.lua
@@ -28,7 +28,7 @@ o.termguicolors = true
 o.timeoutlen = 400
 o.undofile = true
 o.cursorline = true
-
+o.scrolloff = 10
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
